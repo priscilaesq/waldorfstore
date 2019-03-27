d711fb0583d77718add8cec32acf77ab0cf0d7fb
Diana Laura <dianamartinez@MacBook-Pro-de-Laura.local>
2019-03-26 19:53:22 -0700

    jiji

--- src/banners.php
+++ src/banners.php
@@ -4,7 +4,7 @@
 
 <main class="admin-options">
   <div class="container">
-  <h1 class="admin-title"> Banners </h1>
+  <h1 class="admin-title"> Banners JUJUJU </h1>
 <div>
 
 <form method="post" action="banners-upload.php" enctype="multipart/form-data">
