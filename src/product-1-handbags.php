06c04626d8f89b847b9d8c2910de265b4f7cf83f
Priscila Esquer <priscilaesquer@Priscilas-MacBook-Pro.local>
2019-03-27 16:55:17 -0700

    mas cambios

--- src/product-1-handbags.php
+++ src/product-1-handbags.php
@@ -32,7 +32,22 @@
     <article class="col-12 col-sm-4 col-md-offset-1 col-md-5">
       <h3 class="pdetails-title"><?php echo $handbag['product_name'] ?></h3>
       <p class="pdetails-precio"> <?php echo $handbag['price'] ?> </p>
-      <button class="primary-button"> Buy now </button>
+      <br>
+      <form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+      <input type="hidden" name="cmd" value="_xclick">
+      <input type="hidden" name="business" value="eljoshua@live.com">
+      <input type="hidden" name="item_name" value="Donation">
+      <input type="hidden" name="item_number" value="1">
+      <input type="hidden" name="amount" value="9.00">
+      <input type="hidden" name="no_shipping" value="0">
+      <input type="hidden" name="no_note" value="1">
+      <input type="hidden" name="currency_code" value="USD">
+      <input type="hidden" name="lc" value="AU">
+      <input type="hidden" name="bn" value="PP-BuyNowBF">
+      <input type="image" src="https://www.paypal.com/en_AU/i/btn/btn_buynow_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online.">
+      <img alt="" border="0" src="https://www.paypal.com/en_AU/i/scr/pixel.gif" width="1" height="1">
+      </form>
+
       <br>
       <br>
       <p class="free-shipping"> Free shipping & returns </p>
