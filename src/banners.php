e9c1fed8826d1f0130dddfdf0ca0dba79b8d5902
Diana Laura <dianamartinez@MacBook-Pro-de-Laura.local>
2019-03-26 20:58:31 -0700

    Banners

--- src/banners.php
+++ src/banners.php
@@ -1,26 +1,43 @@
 <?php
   include('estructura/header-admin.php');
+  include('funciones.php');
+
+
+  if($_POST['checar'] != '') {
+    $id_banner = registrar_banner();
+    if( $id_banner ) {
+      if(update_imagen('banners',$id_banner,'img')) {
+        $message = "wrong answer";
+          echo "<script type='text/javascript'>alert('$message');</script>";
+      }
+      else {
+        echo 'no se subio';
+      }
+    }
+    else {
+      echo 'no se registro';
+    }
+  }
 ?>
 
 <main class="admin-options">
   <div class="container">
-
-  <h1 class="admin-title"> Banners</h1> 
+  <h1 class="admin-title"> Banners </h1>
 <div>
-
-
-
 
  <section class="subir-foto">
       <h2>Banner management</h2>
-      <form action="subir.php" method="post" enctype="multipart/form-data">
-          <label class="hide-input rose-link upload-new-pic">
+      <form action="banners.php" method="post" enctype="multipart/form-data">
+          <label class="hide-input rose-link upload-new-pic hidden-file-input">
             Upload new picture
-          <input type="file" name="archivo">
+          <input type="file" name="img">
+          <input name="checar" value="si" type="hidden">
           </label>
+          <button class="primary-button"> Upload </button>
           <br>
       </form>
   </section>
+
 
   <section class= "current-photos">
       <div class="row">
@@ -31,7 +48,7 @@
           <article class="uploaded">
             <img class="thumbnail-tiny" src="imgs/home-amelia-spade.png">
             <p>file name here.ext</p>
-            <a class="primary-button" href="eliminar.php?nombre=<?php echo $archivo; ?>">Eliminar</a>
+            <a class="dark-link" href="eliminar.php?nombre=<?php echo $archivo; ?>">Eliminar</a>
             <a href="/banners/<?php echo $archivo; ?>" target="_blank"> <?php echo $archivo; ?></a>
 
           </article>
