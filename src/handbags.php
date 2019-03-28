6b105c39f985df200567022889645c40aae40c6a
Priscila <priscilaesquer.m@gmai.com>
2019-03-27 22:48:18 -0700

    cambios

--- src/handbags.php
+++ src/handbags.php
@@ -19,15 +19,14 @@
     foreach($handbags as $handbag):
       $url = "product-1-handbags.php?id=".$handbag['id'];
   ?>
-      <article class="col-12 col-sm-6 col-md-3">
+      <article class="col-12 col-sm-6 col-md-3 col-lg-3">
       <a href="<?php echo $url ?>"><img class="img-home" src=<?php echo $handbag['image_1'] ?>></a>
       <a href="<?php echo $url ?>"><h3 class="products-title"> <?php echo $handbag['product_name'] ?> </h3></a>
       <p class="products-price"> $<?php echo $handbag['price'] ?> </p>
       </article>
-<?php
+      <?php
   endforeach;
 ?>
-  
 </div>
 </section>
 
